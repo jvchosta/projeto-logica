6e12a677c3c178f2bc64d0700673b57922e23d59
jvchosta <joaovitorcosta.oficial@gmail.com>
2024-10-10 19:07:24 -0300

    entrega final

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 let heroName = "Oil Man"
-let xp = "9987"
-
+let xp = 9875
+let classification
+if (xp > 10000){
+    classification = "Radiante";
+}
+else if (xp > 9000){
+    classification = "Imortal"
+}
+else if (xp > 8000){
+    classification = "Ascendente"
+}
+else if (xp > 7000){
+    classification = "Platina"
+}
+else if (xp > 6000){
+    classification = "Ouro"
+}
+else if (xp > 5000){
+    classification = "Prata"
+}
+else if (xp > 1000){
+    classification = "Bronze"
+}
+else if (xp < 1000){
+    classification = "Ferro"
+}
+console.log("O herói de nome " + heroName + " está no nível " + classification +", com " + xp + " de xp! ;)")
